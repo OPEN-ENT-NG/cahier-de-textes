94d06348e60cba4b2b0c0ba7f38098749918977b
PUEL <vincent.puel@atos.net>
2016-03-01 18:26:08 +0100

    sql script remove comments

--- src/main/resources/sql/002-diary.sql
+++ src/main/resources/sql/002-diary.sql
@@ -4,71 +4,61 @@ DROP TABLE IF EXISTS diary.homework_type;
 DROP TABLE IF EXISTS diary.lesson;
 DROP TABLE IF EXISTS diary.teacher;
 
--- ----------------------------------------------------------------------------
--- Teacher table
--- ----------------------------------------------------------------------------
+
 CREATE TABLE diary.teacher (
-    teacher_id character varying(37), -- teacher's identifier in AF
-    teacher_display_name character varying(120), -- how teacher's name appears on the diary
+    teacher_id character varying(37),
+    teacher_display_name character varying(120),
     PRIMARY KEY (teacher_id)
 );
 
--- ----------------------------------------------------------------------------
--- Lesson table
--- ----------------------------------------------------------------------------
+
 CREATE TABLE diary.lesson (
     lesson_id bigserial,
-    subject_code character varying(20), -- lesson's subject code
-    subject_label character varying(20), -- lesson's subject label
-    school_id character varying(37), -- identifier of the school in the ENT
-    teacher_id character varying(37), -- teacher's identifier in AF
-    audience_type character varying(8), -- enum : 'CLASS' or 'GROUP' 
-    audience_code character varying(20), -- identifier of the class or group in the AF
-    audience_label character varying(20), -- label of the class or group 
-    lesson_title character varying(50), -- lesson's title
-    lesson_room character varying(8), -- room for the lesson
-    lesson_color character varying(6), -- lesson's display color format is RRVVBB (hexadecimal values)
-    lesson_date date, -- lesson date
-    lesson_start_time time, -- lesson start hour
-    lesson_end_time time, -- lesson end hour
-    lesson_description text, -- lesson's description in the html rich editor
-    lesson_annotation text, -- teacher's personal annotation, only him can see it
+    subject_code character varying(20),
+    subject_label character varying(20),
+    school_id character varying(37),
+    teacher_id character varying(37),
+    audience_type character varying(8),
+    audience_code character varying(20),
+    audience_label character varying(20),
+    lesson_title character varying(50),
+    lesson_room character varying(8),
+    lesson_color character varying(6),
+    lesson_date date,
+    lesson_start_time time,
+    lesson_end_time time,
+    lesson_description text,
+    lesson_annotation text,
     PRIMARY KEY (lesson_id),
     CONSTRAINT teacher_id_FK FOREIGN KEY (teacher_id)
         REFERENCES diary.teacher(teacher_id) ON DELETE CASCADE
 );
 
 
--- ----------------------------------------------------------------------------
--- Homework types table
--- ----------------------------------------------------------------------------
 CREATE TABLE diary.homework_type (
     homework_type_id bigserial,
-    school_id character varying(37), -- identifier of the school in the ENT
-    homework_type_label character varying(50), -- homework type's label
-    homework_type_category character varying(15), -- homework type's category. Enum to be determined
+    school_id character varying(37),
+    homework_type_label character varying(50),
+    homework_type_category character varying(15),
     PRIMARY KEY (homework_type_id)
 );
 
 
--- ----------------------------------------------------------------------------
--- Homework table
--- ----------------------------------------------------------------------------
 CREATE TABLE diary.homework (
     homework_id bigserial,
-    subject_code character varying(20), -- homework's subject code
-    subject_label character varying(20), -- homework's subject label
-    school_id character varying(37), -- identifier of the school in the ENT
-    teacher_id character varying(37), -- teacher's identifier in AF
-    audience_type character varying(8), -- enum : 'CLASS' or 'GROUP' 
-    audience_code character varying(20), -- identifier of the class or group in the AF
-    audience_label character varying(20), -- label of the class or group 
-    lesson_id bigint, -- identifier of the lesson to which the homework is attached (null if free homework)
-    homework_title character varying(50), -- homework's title
-    homework_description text, -- homework's description in the html rich editor
-    homework_type_id integer,  -- homework's type
-    homework_due_date date,  -- homework's due date
-    homework_color character varying(6), -- homework's display color format is RRVVBB (hexadecimal values)
+    subject_code character varying(20),
+    subject_label character varying(20),
+    school_id character varying(37),
+    teacher_id character varying(37),
+    audience_type character varying(8),
+    audience_code character varying(20),
+    audience_label character varying(20),
+    lesson_id bigint,
+    homework_title character varying(50),
+    homework_description text,
+    homework_type_id integer,
+    homework_due_date date,
+    homework_color character varying(6),
     PRIMARY KEY (homework_id),
     CONSTRAINT lesson_id_fk FOREIGN KEY (lesson_id)
         REFERENCES diary.lesson(lesson_id) ON DELETE CASCADE, 
@@ -78,17 +68,14 @@ CREATE TABLE diary.homework (
         REFERENCES diary.teacher(teacher_id) ON DELETE CASCADE
 );
 
--- -----------------------------------------------------------------------------
--- Attachments table (stored in the diary storage space)
--- -----------------------------------------------------------------------------
 CREATE TABLE diary.attachment (
     attachment_id bigserial,
     attachment_description text,
-    attachment_file_name character varying(255) NOT NULL, -- file name displayed to the users
-    attachment_file_id character varying(37) NOT NULL, -- identifier of the file in the storage api
-    attachment_file_size bigint, -- file size in octets
-    homework_id bigint, -- identifier of the homework to which the file is attached, null if attached to a lesson
-    lesson_id bigint, -- identifier of the lesson to which the file is attached, null if attached to a homework
+    attachment_file_name character varying(255) NOT NULL,
+    attachment_file_id character varying(37) NOT NULL,
+    attachment_file_size bigint,
+    homework_id bigint,
+    lesson_id bigint,
     PRIMARY KEY (attachment_id),
 	  CONSTRAINT lesson_id_fk FOREIGN KEY (lesson_id)
         REFERENCES diary.lesson(lesson_id), 
