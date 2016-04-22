11db0cdcfa90bc6e5ddbe949aa23ce2588634cd3
a457593 <vincent.puel@atos.net>
2016-04-22 16:02:52 +0200

    Publish Lesson

--- src/main/java/fr/openent/diary/services/LessonServiceImpl.java
+++ src/main/java/fr/openent/diary/services/LessonServiceImpl.java
@@ -275,6 +275,7 @@ public class LessonServiceImpl extends SqlCrudService implements LessonService {
 
         lessonObject.removeField("audience_type");
         lessonObject.removeField("audience_name");
+        lessonObject.removeField("lesson_id");
     }
 
     /**
