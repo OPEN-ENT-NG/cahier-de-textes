a39f48d15a706547848d3f89668d27161242f191
PUNZANO <thomas.punzano@atos.net>
2017-07-20 11:25:06 +0200

    Ajout scenario homework

--- test/src/calendar/create-homework.spec.js
+++ test/src/calendar/create-homework.spec.js
@@ -3,6 +3,7 @@ var constants = require('../constants');
 var loginService = require('../common/login.service');
 var lessonService = require('../common/lesson.service.js');
 var pgService = require('../common/pg.service.js');
+var homeworkService = require('../common/homework.service.js');
 // spec.js
 describe('Protractor Calendar', function() {
 
@@ -17,11 +18,41 @@ describe('Protractor Calendar', function() {
     });
 
 
-    it('Verify Calendar homework creation',function(){
-        /*element.all(by.css('input[ng-model="item.selected"]')).then(function(items) {
+    it('Verify Calendar has no homework',function(){
+        element.all(by.css('input[ng-model="item.selected"]')).then(function(items) {
             expect(items.length).toBe(0);
         });
-        */
+    });
+
+
+    it('Sould create draft homework from button', function() {
+        homeworkService.createHomeworkFromButton(true,constants.momentWeek.format('DD/MM/YYYY'));
+    });
+
+    it('Sould create public homework from button', function() {
+        homeworkService.createHomeworkFromButton(false,constants.momentWeek.format('DD/MM/YYYY'));
+    });
+
+    it('Should delete selected homework', function() {
+        homeworkService.deleteHomeworkFromBan()
+    });
+
+    it('Should create public homework from Calendar', function() {
+        homeworkService.createHomeworkFromCal(24, false)
+    });
+
+    it('Should drag and drop', function() {
+        homeworkService.dragAndDropLastHomework(25);
+    });
+
+    it('Should delete homework from Calendar', function() {
+        homeworkService.deleteHomeworkFromCal()
+    });
+
+    it('Verify Calendar homework creation',function(){
+        element.all(by.css('input[ng-model="item.selected"]')).then(function(items) {
+            expect(items.length).toBe(0);
+        });
     });
 
     it('Should disconnect', function() {
---
+++ test/src/common/homework.service.js
@@ -0,0 +1,134 @@
+var dragAndDrop = require('html-dnd').code;
+
+var srv = {
+    createHomeworkFromCal: (itemNumber, isDraft, matter) => {
+        browser.waitForAngular();
+        browser.sleep(1000);
+        // URL: #/createLessonView/timeFromCalendar
+        element.all(by.css('[ng-repeat="timeslot in day.timeSlots.all"]')).get(itemNumber).click();
+        browser.waitForAngular();
+        srv.selectAudience(4);
+        browser.sleep(400);
+        srv.setTitleDescriptionLesson("Titre de la sceance",
+            "Description de la sceance");
+        if (matter){
+            srv.createNewMatter(matter);
+        }
+        browser.executeScript('window.scrollTo(0,0);');
+        $('[data-ng-class="{\'selected\': data.tabSelected === \'homeworks\'}"]').click();
+        browser.sleep(200);
+        $('[data-ng-click="addHomeworkToLesson(editLessonCtrl.lesson)"]').click();
+        browser.sleep(400);
+        srv.setTitleDescriptionHomework("Titre du devoir maison", "Description du devoir maison");
+        srv.saveHomework(isDraft);
+    },
+    createHomeworkFromButton: (isDraft, day,matter) => {
+        browser.waitForAngular();
+        browser.sleep(200);
+        // URL: #/createLessonView/timeFromCalendar
+        $('[workflow="diary.createFreeHomework"]').click();
+        browser.waitForAngular();
+        srv.selectAudience(4);
+        srv.setTitleDescriptionHomework("Titre du travail a faire",
+            "Description du travail a faire");
+        if (matter){
+            srv.createNewMatter(matter);
+        }
+        browser.executeScript('window.scrollTo(0,0);');
+        srv.setDay(day);
+        // srv.setType(type);
+        srv.saveHomework(isDraft);
+    },
+    selectAudience : (nbAudience)=>{
+        browser.waitForAngular();
+        $('ent-dropdown>div:nth-of-type(1)>div:nth-of-type(1)>span').click();
+        browser.sleep(200);
+        $('ent-dropdown>div:nth-of-type(1)>div:nth-of-type(2)>div:nth-of-type(1)>div:nth-of-type('+nbAudience+')>span').click();
+    },
+    setTitleDescriptionHomework : (title,description)=>{
+        browser.waitForAngular();
+        $('input[ng-model="homework.title"]').click();
+        $('input[ng-model="homework.title"]').clear().sendKeys(title);
+        element.all(by.css('[contenteditable]')).get(0).clear().sendKeys(description);
+        browser.executeScript('window.scrollTo(0,200);');
+    },
+    setTitleDescriptionLesson : (title,description)=>{
+        browser.waitForAngular();
+        $('[ng-model="editLessonCtrl.lesson.title"]').click();
+        $('[ng-model="editLessonCtrl.lesson.title"]').clear().sendKeys(title);
+        element.all(by.css('[contenteditable]')).get(0).clear().sendKeys(description);
+        browser.executeScript('window.scrollTo(0,200);');
+    },
+    setDay : (day)=>{
+        $('[ng-model="newItem.date"]').click();
+        browser.sleep(100);
+        let dateInputElement = $('[ng-model="newItem.date"]');
+        dateInputElement.click().clear();
+        for (var i=0;i<10;i++){
+            dateInputElement.sendKeys(protractor.Key.BACK_SPACE);
+        }
+        dateInputElement.sendKeys(day);
+    },
+    saveHomework:(isDraft)=>{
+      //button save
+      browser.waitForAngular();
+      let nbButton = isDraft ? 1 : 0;
+      browser.sleep(200);
+      browser.executeScript('window.scrollTo(0,0);');
+      browser.sleep(200);
+      element.all(by.css('button.right-magnet[type="submit"]')).get(nbButton).click();
+      browser.sleep(500);
+    },
+    // setType : (type)=>{
+    //   browser.waitForAngular();
+    //   element(by.css('select[ng-model="homework.type"]')).click();
+    //   browser.sleep(200);
+    //   element(by.css('ent-dropdown>div:nth-of-type(1)>div:nth-of-type(2)>div:nth-of-type(1)>div:nth-of-type('+nbAudience+')>span')).click();
+    // },
+    deleteHomeworkFromBan: ()=>{
+        browser.waitForAngular();
+        browser.sleep(400);
+        browser.executeScript('window.scrollTo(0,0);');
+        $('[ng-click="toggleShowHwDetail(day)"]').click();
+        browser.sleep(400);
+        $$('[ng-model="dailyEvent.selected"]').get(0).click();
+        $$('[ng-model="dailyEvent.selected"]').get(1).click();
+        browser.sleep(400);
+        $('[ng-click="showConfirmPanel(\'confirm-delete\')"]').click();
+        browser.sleep(400);
+        $('[ng-click="deleteSelectedItems()"]').click();
+    },
+    deleteHomeworkFromCal: ()=>{
+      browser.waitForAngular();
+      browser.sleep(400);
+      $('[ng-if="item.calendarType!==\'shadow\'"]').click();
+      browser.executeScript('window.scrollTo(0,0);');
+      $('[data-ng-class="{\'selected\': data.tabSelected === \'homeworks\'}"]').click();
+      browser.sleep(200);
+      browser.executeScript('window.scrollTo(0,document.body.scrollHeight);');
+      $('[ng-click="showConfirmPanel(\'confirm-delete-homeworklesson\', {homework: homework, lesson : editLessonCtrl.lesson })"]').click();
+      $('[ng-click="deleteHomeworkAndCloseConfirmPanel(confirmPanel.item.homework, confirmPanel.item.lesson)"]').click();
+      browser.executeScript('window.scrollTo(0,0);');
+      $('[ng-click="back()"]').click();
+      $('[ng-model="item.selected"]').click();
+      browser.sleep(400);
+      $('[ng-click="showConfirmPanel(\'confirm-delete\')"]').click();
+      browser.sleep(400);
+      $('[ng-click="deleteSelectedItems()"]').click();
+    },
+    dragAndDropLastHomework: (itemNumber,isDraft)=>{
+      browser.waitForAngular();
+      $('quick-search:nth-of-type(2)>div:nth-of-type(1)').click();
+      browser.sleep(1000);
+      var draggable = $('article.quick-search-card');
+      var droppable = element.all(by.css('[ng-repeat="timeslot in day.timeSlots.all"]')).get(itemNumber);
+      browser.executeScript(dragAndDrop, draggable, droppable, 5, 5);
+      browser.sleep(200);
+      browser.waitForAngular();
+      srv.selectAudience(4);
+      srv.setTitleDescriptionHomework("Titre du devoir maison modifié", "Description du devoir maison modifié");
+      srv.saveHomework(isDraft);
+    }
+};
+
+module.exports = srv;
