9f48b57ddc558658d3ef9e791ae513349fea6c8f
remy.seuret <remy.seuret@cgi.com>
2019-02-25 15:10:31 +0100

    Commit init update

--- src/main/java/fr/openent/diary/controllers/InitController.java
+++ src/main/java/fr/openent/diary/controllers/InitController.java
@@ -10,33 +10,36 @@ import fr.wseduc.security.SecuredAction;
 import fr.wseduc.webutils.Either;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.entcore.common.controller.ControllerHelper;
 import org.entcore.common.http.filter.ResourceFilter;
 import org.entcore.common.http.response.DefaultResponseHandler;
+import org.entcore.common.user.UserUtils;
 
 public class InitController extends ControllerHelper {
 
     private InitService initService;
+
     public InitController(InitService initService) {
         this.initService = initService;
     }
 
     @Get("/init")
-    @SecuredAction(value = "", type = ActionType.RESOURCE)
-    @ResourceFilter(AccessOwnData.class)
+    @SecuredAction(value = "", type = ActionType.AUTHENTICATED)
     public void initHomeworksType(final HttpServerRequest request) {
-        String startDate = request.getParam("startDate");
-        String endDate = request.getParam("endDate");
-        String type = request.getParam("type");
-        String typeId = request.getParam("typeId");
-
         initService.init(new Handler<Either<String, JsonObject>>() {
             @Override
             public void handle(Either<String, JsonObject> event) {
-
+                if (event.isRight()) {
+                    renderJson(request, event.right().getValue());
+                } else {
+                    log.error("Error when init");
+                    badRequest(request);
+                }
             }
         });
     }
+
 }
 
