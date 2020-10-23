76691f180414fd61ec413d7252ef0959b1dfc74a
quentin.picard <quentin.picard.18@gmail.com>
2020-10-23 14:24:10 +0200

    test(): Add example of asynchronous test on SimpleTest

--- src/test/java/fr/openent/diary/test/SimpleTest.java
+++ src/test/java/fr/openent/diary/test/SimpleTest.java
@@ -1,15 +1,37 @@
 package fr.openent.diary.test;
 
+import fr.wseduc.webutils.Either;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 @RunWith(VertxUnitRunner.class)
 public class SimpleTest {
-    Vertx vertx = Vertx.vertx();
+    Vertx vertx;
+    HttpServer server;
+
+    @Before
+    public void setUp(TestContext context) {
+        vertx = Vertx.vertx();
+        // Register the context exception handler
+        vertx.exceptionHandler(context.exceptionHandler());
+
+        server = vertx.createHttpServer().requestHandler(req -> req.response().end("test")).
+                        listen(8090, context.asyncAssertSuccess());
+    }
+
+    @After
+    public void after(TestContext context) {
+        vertx.close(context.asyncAssertSuccess());
+    }
 
     @Test
     public void testReadI18nFile(TestContext ctx) {
@@ -22,5 +44,33 @@ public class SimpleTest {
                 async.complete();
             }
         });
+    }
+
+    @Test
+    public void testAsyncSucceed(TestContext ctx) {
+        Async async = ctx.async();
+        asyncManagement(true, res -> {
+            ctx.assertTrue(res.isRight());
+            ctx.assertEquals("ok", res.right().getValue());
+            async.complete();
+        });
+    }
+
+    @Test
+    public void testAsyncFailed(TestContext ctx) {
+        Async async = ctx.async();
+        asyncManagement(false, res -> {
+            ctx.assertTrue(res.isLeft());
+            ctx.assertEquals("ko", res.left().getValue());
+            async.complete();
+        });
+    }
+
+    private void asyncManagement(boolean var, Handler<Either<String, String>> handler) {
+        if (var) {
+            handler.handle(new Either.Right<>("ok"));
+            return;
+        }
+        handler.handle(new Either.Left<>("ko"));
     }
 }
