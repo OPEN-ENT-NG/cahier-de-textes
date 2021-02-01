4cff1e59eeb1453e2d4a20b872bacb754ec99cc6
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2021-02-01 15:42:40 +0100

    feat: #41835, Upgrade angular version

--- src/main/resources/public/ts/directives/staticInclude.ts
+++ src/main/resources/public/ts/directives/staticInclude.ts
@@ -3,7 +3,7 @@ import { ng } from 'entcore';
 export const StaticInclude = ng.directive('staticInclude', function($http, $templateCache, $compile) {
     return function(scope, element, attrs) {
         let templatePath = attrs.staticInclude;
-        $http.get(templatePath, { cache: $templateCache }).success(function(response) {
+        $http.get(templatePath, { cache: $templateCache }).then(function(response) {
             let contents = element.html(response).contents();
             $compile(contents)(scope);
         });
