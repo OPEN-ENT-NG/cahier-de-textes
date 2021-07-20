9b7110fb892fc07037f21bf41906bc488ac117a7
quentin.picard <quentin.picard.18@gmail.com>
2021-07-20 18:28:09 +0200

    fix(archive): #MCDT-551 set columns nullable

---
+++ src/main/resources/sql/028-MCDT-551-diary-archive-notebook-null.sql
@@ -0,0 +1,7 @@
+ALTER TABLE diary.notebook_archive ALTER COLUMN audience_label DROP NOT NULL;
+ALTER TABLE diary.notebook_archive ALTER COLUMN teacher_id DROP NOT NULL;
+ALTER TABLE diary.notebook_archive ALTER COLUMN teacher_first_name DROP NOT NULL;
+ALTER TABLE diary.notebook_archive ALTER COLUMN teacher_last_name DROP NOT NULL;
+ALTER TABLE diary.notebook_archive ALTER COLUMN audience_label DROP NOT NULL;
+ALTER TABLE diary.notebook_archive ALTER COLUMN subject_label DROP NOT NULL;
+ALTER TABLE diary.notebook_archive ALTER COLUMN archive_school_year DROP NOT NULL;
