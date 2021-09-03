1b4eeb6d3d6b1dda729a7e2af5675c903bba058d
Flavien Martineau <45894263+flmartineau@users.noreply.github.com>
2021-09-03 17:32:32 +0200

    feat(): #MCDT-555 add security to search group/users APIs

--- src/main/java/fr/openent/diary/security/workflow/SearchRight.java
+++ src/main/java/fr/openent/diary/security/workflow/SearchRight.java
@@ -8,9 +8,13 @@ import io.vertx.core.http.HttpServerRequest;
 import org.entcore.common.http.filter.ResourcesProvider;
 import org.entcore.common.user.UserInfos;
 
+import java.util.*;
+
 public class SearchRight implements ResourcesProvider {
     @Override
     public void authorize(HttpServerRequest httpServerRequest, Binding binding, UserInfos user, Handler<Boolean> handler) {
-        handler.handle(WorkflowUtils.hasRight(user, "diary.search"));
+        String structureId = httpServerRequest.params().get("structureId");
+        List<String> structures = user.getStructures();
+        handler.handle(structures.contains(structureId) && WorkflowUtils.hasRight(user, WorkflowUtils.DIARY_SEARCH));
     }
 }
