4dbdc20ccfd52f55fb1a8aa4aa01c8ff75ffc123
Benard Bastien <bastien.benard@cgi.com>
2022-04-22 17:23:00 +0200

    fix(security): #MCDT-583 add autorisation to NotebookController

---
+++ src/main/java/fr/openent/diary/core/constants/Field.java
@@ -0,0 +1,17 @@
+package fr.openent.diary.core.constants;
+
+public class Field {
+    public static final String ERROR = "error";
+    public static final String STRUCTURE_ID = "structure_id";
+    public static final String IS_PUBLISHED = "is_published";
+    public static final String VISA = "visa";
+    public static final String AUDIENCE_ID = "audience_id";
+    public static final String TEACHER_ID = "teacher_id";
+    public static final String SUBJECT_ID = "subject_id";
+    public static final String END_AT = "end_at";
+    public static final String START_AT = "start_at";
+    public static final String ORDERVISA = "orderVisa";
+    public static final String PAGE = "page";
+    public static final String LIMIT = "limit";
+    public static final String OFFSET = "offset";
+}
