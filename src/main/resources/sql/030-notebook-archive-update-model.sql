e975217bf88c597c5d9ec4df18bee7a5a71f484b
Jinhda Douang <Jinhda.douangphrachanh@cgi.com>
2024-07-29 16:55:07 +0200

    fix(notebook): update notebook archive sql varchar length

---
+++ src/main/resources/sql/030-notebook-archive-update-model.sql
@@ -0,0 +1,4 @@
+ALTER TABLE diary.notebook_archive ALTER COLUMN teacher_first_name TYPE VARCHAR(100);
+ALTER TABLE diary.notebook_archive ALTER COLUMN teacher_last_name TYPE VARCHAR(100);
+ALTER TABLE diary.notebook_archive ALTER COLUMN audience_label TYPE VARCHAR(100);
+ALTER TABLE diary.notebook_archive ALTER COLUMN subject_label TYPE VARCHAR(100);
