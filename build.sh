ee62d8188c47dd6373d8a011a66ddcf55951283f
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-10-30 17:16:41 +0100

    ci: use FRONT_BRANCH to specify entcore version

--- build.sh
+++ build.sh
@@ -32,7 +32,12 @@ clean () {
 buildNode () {
   #jenkins
   echo "[buildNode] Get branch name from jenkins env..."
-  BRANCH_NAME=`echo $GIT_BRANCH | sed -e "s|origin/||g"`
+  if [ ! -z "$FRONT_BRANCH" ]; then
+    echo "[buildNode] Get tag name from jenkins param... $FRONT_BRANCH"
+    BRANCH_NAME="$FRONT_BRANCH"
+  else
+    BRANCH_NAME=`echo $GIT_BRANCH | sed -e "s|origin/||g"`
+  fi
   if [ "$BRANCH_NAME" = "" ]; then
     echo "[buildNode] Get branch name from git..."
     BRANCH_NAME=`git branch | sed -n -e "s/^\* \(.*\)/\1/p"`
